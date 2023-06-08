8a595be99292a99583827c5949a475b6b40ff5e7
RAMSES <95148596+RAMSES-DG@users.noreply.github.com>
2023-06-08 07:08:23 +0300

    Add files via upload

---
+++ data/lists.py
@@ -0,0 +1,16 @@
+choises_list="""
+
+@ help list -
+            - you can run any command here
+
+ command              | service
+----------------------------------------
+|   2   |             | to encrypt 2 letters text
+|   3   |             | to encrypt 3 letters text
+|   4   |             | to encrypt 4 letters text
+|  exit |             | to exit chifre tool
+|  help |             | to show this help list
+"""
+
+
+
